99f11dfcf6e50c996dbc98d05452f6773ed4e882
ATpredator <antoniobr.dev010@gmail.com>
2022-04-11 16:43:46 -0400

    update-cm

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,16 +1,16 @@
-import { useHistory } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-import ProjectForm from '../project/ProjectForm'
+import ProjectForm from '../project/ProjectForm';
 
-import styles from './NewProject.module.css'
+import styles from './NewProject.module.css';
 
 function NewProject() {
-  const history = useHistory()
+  const navigate = useNavigate();
 
   function createPost(project) {
     // initialize cost and services
-    project.cost = 0
-    project.services = []
+    project.cost = 0;
+    project.services = [];
 
     fetch('http://localhost:5000/projects', {
       method: 'POST',
@@ -21,8 +21,8 @@ function NewProject() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        history.push('/projects', { message: 'Projeto criado com sucesso!' })
-      })
+        navigate('projects', { message: 'Projeto criado com sucesso!' });
+      });
   }
 
   return (
@@ -31,7 +31,7 @@ function NewProject() {
       <p>Crie seu projeto para depois adicionar os serviços</p>
       <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
     </div>
-  )
+  );
 }
 
-export default NewProject
+export default NewProject;
